cf1e91acd96e6aadf577af64e42e3f4e0e54b0a9
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-28 11:35:48 +0530

    First commit

---
+++ FormulaAirline.API/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace FormulaAirline.API.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string PassangerName { get; set; } = "";
+
+        public string PassportNb { get; set; } = "";
+
+        public string From { get; set; } = ""; 
+        public string To { get; set; } = ""; 
+
+        public int Status { get; set; }
+
+    }
+}
